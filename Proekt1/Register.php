ceed3971337d7ea58aa0186eac0a89fc0bd05808
user7623 <ddroid172@gmail.com>
2020-04-11 13:06:36 +0200

    homepage

--- Proekt1/Register.php
+++ Proekt1/Register.php
@@ -1,3 +1,31 @@
+<?php
+$msg = '';
+$pass = filter_input(INPUT_POST, 'Pass');
+$pass_confirm = filter_input(INPUT_POST, 'CPass');
+$email = filter_input(INPUT_POST, 'Ename', FILTER_VALIDATE_EMAIL);
+//proveri dali e vnesen validen meil
+
+if($email != true && !empty($pass))
+{
+    $msg = 'Please input valid email';
+}
+
+//proveri dali e unikatno username
+
+//proveri dali se sovpagjaad passwords
+if($pass != $pass_confirm)
+{
+    $msg = 'password inputs not matching';
+}
+
+// ako se e vo red prati meil 
+
+if($msg == '' && !empty($pass))
+{
+    $msg = 'cekam_avtentikacija';
+}
+?>
+
 <!DOCTYPE html>
 
 <html>
@@ -10,28 +38,41 @@
     <body> 
             <h1>Registration form</h1>
             <div class="register">
-                <form method="POST" action="Login.php" class="regform">
-                    <label>First name: </label> <br>
-                    <input type="text" name="Fname" class="Fnamec" placeholder="Enter first name" required> <br> <br>
+                <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" class="regform">
+				<?php if($msg == 'cekam_avtentikacija') : ?>
+                <p>Check inbox for conformation! NOW!!!</p>
+                <div>
+                   <p class="para">Back to Login <a class="linke" href="Login.php">Login page</a></p> <hr>
+                </div>
+				<?php else : ?>	
+					<?php if($msg != '') : ?>
+						<p class="error"><?php echo $msg ?></p>
+						<?php endif; ?>
+						<label>First name: </label> <br>
+						<input type="text" name="Fname" class="Fnamec" placeholder="Enter first name" required> <br> <br>
 
-                    <label>Last name: </label> <br>
-                    <input type="text" name="Lname" class="Fnamec" placeholder="Enter last name" required> <br> <br>
+						<label>Last name: </label> <br>
+						<input type="text" name="Lname" class="Fnamec" placeholder="Enter last name" required> <br> <br>
+						
+						<label>Username: </label> <br>
+						<input type="text" name="Uname" class="Fnamec" placeholder="Enter username" required> <br> <br>
 
-                    <label>Email: </label> <br>
-                    <input type="text" name="Ename" class="Fnamec" placeholder="Enter email address" required> <br> <br>
+						<label>Email: </label> <br>
+						<input type="text" name="Ename" class="Fnamec" placeholder="Enter email address" required> <br> <br>
 
-                    <label>Password: </label> <br>
-                    <input type="password" name="Pass" class="Fnamec" placeholder="Enter password" required> <br> <br>
+						<label>Password: </label> <br>
+						<input type="password" name="Pass" class="Fnamec" placeholder="Enter password" required> <br> <br>
 
-                    <label>Confirm password: </label> <br>
-                    <input type="password" name="CPass" class="Fnamec" required> <br> <br>
+						<label>Confirm password: </label> <br>
+						<input type="password" name="CPass" class="Fnamec" placeholder="Confirm password" required> <br> <br>
 
-                    <label>Select your gender: </label> <br>
-                    <input type="radio" name="gender" value="male" required> Male
-                    <input type="radio" name="gender" value="female"> Female <br> <br>
+						<label>Select your gender: </label> <br>
+						<input type="radio" name="gender" value="male" required> Male
+						<input type="radio" name="gender" value="female"> Female <br> <br>
 
-                    <input type="submit" class="sub" value="Register">
-                </form>
+						<input type="submit" class="sub" value="Register">
+					<?php endif; ?>
+					</form>
             </div>
        
     </body>
